703aca912914472d040f7b23a7accdba96f720e6
Emilio Wong <emilews.ew@gmail.com>
2018-10-17 14:52:55 -0700

    Added printing loop for array in memory

--- Promedio/Promedio.asm
+++ Promedio/Promedio.asm
@@ -6,6 +6,7 @@
 
 segment .bss
 	arreglo_enteros  resb 200         ;50 casillas de 4 bytes c/u
+	elementos resb 4				  ;numero de elementos
 
 section .text
 	global _start:
@@ -19,14 +20,15 @@ _start:
     ;BLOQUE de INICIALIZACION
 	pop eax			;obtenemos nombre de programa
 	dec ecx			;restamos 1 al numero de argumentos
+	mov elementos, ecx
 	mov edx, 0		;ponemos en 0 EDX
 	mov esi,arreglo_enteros   ;la direccion de 'arreglo_enteros' a ESI
 
     ;CICLO DE EXTRACCION DE ARGUMENTOS
 ciclo:
-	pop eax				;sacamos direccion de argumento del stack
-	call atoi			;lo convertimos a entero de 4 bytes
-	mov [esi+edx*4],eax	;lo guardamos en el arreglo_enteros
+	pop eax					;sacamos direccion de argumento del stack
+	call atoi				;lo convertimos a entero de 4 bytes
+	mov [esi+edx*4],eax		;lo guardamos en el arreglo_enteros
 	inc edx					;incrementamos el indice de arreglo_enteros
 	dec ecx					;decrementamos numero de argumentos por procesar
 	cmp ecx,0				;preguntamos si ya no tenemos argumentos
@@ -34,16 +36,20 @@ ciclo:
 
     ;CICLO DE IMPRESION
 ciclo_impresion:
-    inc ecx
-    mov eax, [esi+ecx*4]    ;Movemos el valor en arreglo hacia eax
-    dec edx
-    call sprintLF
-    cmp edx, '0'
-    jz salir
-    jnz ciclo_suma
+	mov ebx, [esi+edx*4]
+	dec edx
+	add eax,ebx
+	cmp ecx, edx
+	je imprimir
+	jne ciclo_impresion
+
 
 ciclo_suma:
 
     ;SALIDA
 salir:
 	jmp quit
+
+imprimir:
+	call iprintLF
+	call salir
